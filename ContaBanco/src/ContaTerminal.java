ea00e2d701135240c3ce7cb5132f270000825f52
Everton Ferraz <evertonferrazo@gmail.com>
2024-08-13 20:31:36 -0300

    Desafio ContaBanco DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponíel para saque.");
+
     }
 }
